552d8df321d174a3f6e4738582dbe8628d58cacf
Kareem Ibrahim <kareem315@gmail.com>
2021-03-31 23:00:27 -0400

    Add basic port listening on port 8080

--- server/app/server.js
+++ server/app/server.js
@@ -1,1 +1,9 @@
 // Set up default listening port
+import express from 'express'
+
+const PORT = 8080;
+
+// init app
+let app = express();
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
