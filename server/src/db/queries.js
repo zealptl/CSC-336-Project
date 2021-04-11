1205999ffaf74c0859d84e4aa8bde721484ed911
Kareem Ibrahim <kareem315@gmail.com>
2021-04-11 16:04:00 -0400

    Add more common queries

--- server/src/db/queries.js
+++ server/src/db/queries.js
@@ -1,4 +1,19 @@
 // Default list of common queries
 export default {
-    getUser: 'SELECT ? FROM User;',
+    getUsers: `SELECT ? FROM Users;`,
+    insertUsers: `INSERT INTO Users (email, firstName, lastName, password, active)
+                  VALUES (?, ?, ?, ?, ?);`,
+    getGroups: `SELECT ? FROM Groups;`,
+    insertGroups: `INSERT INTO Groups (groupName, active)
+                   VALUES (?, ?);`,
+    getGroupsForUser: `SELECT groupID, userEmail FROM GroupUsers
+                       WHERE userEmail = ?, active = TRUE;`,
+    getMessageFromUser: `SELECT groupID, body, timeSent FROM Message
+                         WHERE userEmail = ?;`,
+    getMessageFromGroup: `SELECT userEmail, body, timeSent FROM Message
+                          WHERE groupID = ?;`,
+    getTaskFromGroup: `SELECT task, status, userEmail, createTime FROM TASK
+                       WHERE groupID = ? AND active = TRUE;`,
+    getReplyFromMessage: `SELECT userEmail, body, timeSent FROM Reply
+                          WHERE messageID = ?;`,
 }
