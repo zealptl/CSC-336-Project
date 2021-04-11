cc2613a1519458a33c530e8d64e331c2750c372d
zealptl <zealpatel06@gmail.com>
2021-04-11 10:06:09 -0400

    change group item

--- client/src/components/GroupListItem.js
+++ client/src/components/GroupListItem.js
@@ -6,30 +6,30 @@ const useStyles = makeStyles((theme) => ({
 		display: 'flex',
 		flexDirection: 'row',
 		margin: theme.spacing(2, 1, 2, 1),
-
+		alignItems: 'center',
 		borderRadius: '0px 10px 10px 0px',
 		transition: 'all 0.2s ease',
 		cursor: 'pointer',
 		'&:hover, &:focus': {
 			background: theme.palette.primary.light,
+
+			'& $bar': {
+				background: theme.palette.primary.main,
+			},
 		},
 	},
 	bar: {
-		height: '45px',
+		height: '50px',
 		width: '3px',
 		background: theme.palette.component.dark,
 		transition: 'all 0.2s ease',
-
-		'&:hover, &:focus': {
-			background: theme.palette.primary.main,
-		},
 	},
 	avatar: {
 		background: theme.palette.primary.main,
 		marginLeft: theme.spacing(1),
 	},
 	groupName: {
-		textOverflow: 'ellipsis',
+		overflow: 'ellipsis',
 		width: '75%',
 		marginLeft: theme.spacing(1),
 	},
