62204abd5cf4649afd6d9b23142bc0f8821c6575
Kareem Ibrahim <kareem315@gmail.com>
2021-04-12 14:45:39 -0400

    Add two new routes for `GET` for Groups * Imports must have .js extension for type: module

--- server/src/routes/group.js
+++ server/src/routes/group.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createGroup } from './controllers/GroupControllers/createGroup';
-import { getGroups } from './controllers/GroupControllers/getGroups';
+import { createGroup } from './controllers/GroupControllers/createGroup.js';
+import { getGroups } from './controllers/GroupControllers/getGroups.js';
 
 const router = express.Router();
 router.post('/', createGroup);
-router.get('/user/:groupName', getGroups); // get the groups the user is in
+router.get('/user/getGroupByName/:groupName', getGroups.getGroupByName); // get the groups the user is in
+router.get('/user/getGroupByID/:groupID', getGroups.getGroupByID);
 
 export default router;
